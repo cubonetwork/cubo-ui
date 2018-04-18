8d8a255920b11f6e66f7163e94eaa6b65c42f217
LFeh <hi@felipefialho.com>
2018-04-18 15:41:39 -0300

    Add basic styles to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.styl']
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./app.component.styl'],
+  template: `
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+  `
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent { }
