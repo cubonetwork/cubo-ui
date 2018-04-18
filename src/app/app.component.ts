496f7da182728c107ff04525614d27152084edeb
LFeh <hi@felipefialho.com>
2018-04-18 15:18:06 -0300

    Add basic css config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
   title = 'app';
