6c7d4fe863ad998448dbfb425971a341b99a2c2b
LFeh <hi@felipefialho.com>
2018-04-19 20:19:14 -0300

    Add example tab

--- src/app/pages/avatar/avatar.component.ts
+++ src/app/pages/avatar/avatar.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-avatar',
   template: `
-    <p>
-      avatar works!
-    </p>
+    <mat-tab-group>
+      <mat-tab label="Exemplo">Content 1</mat-tab>
+      <mat-tab label="API">Content 2</mat-tab>
+    </mat-tab-group>
   `,
   styleUrls: ['./avatar.component.scss']
 })
