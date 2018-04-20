51d08d4748dce69e2f9a1440a23fcd41d66886ec
LFeh <hi@felipefialho.com>
2018-04-20 11:16:16 -0300

    Remove wrong item

--- src/app/pages/avatar/avatar.component.ts
+++ src/app/pages/avatar/avatar.component.ts
@@ -4,11 +4,10 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-avatar',
   template: `
     <mat-tab-group>
-      <mat-tab label="Exemplo">Content 1</mat-tab>
+      <mat-tab label="Exemplo"></mat-tab>
       <mat-tab label="API">Content 2</mat-tab>
     </mat-tab-group>
   `,
-  styleUrls: ['./avatar.component.scss']
 })
 export class PageAvatarComponent implements OnInit {
 
