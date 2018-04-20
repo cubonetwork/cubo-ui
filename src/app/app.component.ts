12b8a2af12d1ebe133068fe281cb77701e9edc41
CarolPC <carolina.pascale.c@gmail.com>
2018-04-20 17:16:02 -0300

    Add colapse with hard-coded code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,15 @@ import {
   ViewEncapsulation,
   ChangeDetectionStrategy
 } from '@angular/core'
+import * as hljs from 'highlight.js';
 
 @Component({
   selector: 'app',
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  styleUrls: ['./app.component.scss'],
+  styleUrls: [
+    './app.component.scss'
+  ],
   template: `
     <header class="header">
       <a class="header__logo">
@@ -18,7 +21,7 @@ import {
     <main class="main">
       <div class="container">
         <nav class="navbar">
-          <mat-nav-list>
+          <mat-nav-list class="nav-list">
             <a mat-list-item routerLink="/avatar" [routerLinkActive]="['navbar--active']" [routerLinkActiveOptions]="{exact: true}">Avatar</a>
             <mat-divider></mat-divider>
             <a mat-list-item>Card</a>
@@ -27,10 +30,101 @@ import {
           </mat-nav-list>
         </nav>
         <article class="content">
-          <router-outlet></router-outlet>
+          <mat-tab-group>
+            <mat-tab label="Example">
+              <mat-card-content class="tab-content">
+                <article class="description">
+                  <p>
+                    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi,
+                    id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.";
+                  </p>
+                </article>
+
+                <mat-accordion>
+                  <mat-expansion-panel>
+                    <mat-expansion-panel-header class="panel__header">
+                      View Code
+                    </mat-expansion-panel-header>
+                    <mat-tab-group>
+                      <mat-tab label="HTML">
+                        <pre class="code">
+                          <code [innerHtml]="html_content"></code>
+                        </pre>
+                      </mat-tab>
+                      <mat-tab label="TS">
+                        <pre class="code">
+                          <code [innerHtml]="ts_content"></code>
+                        </pre>
+                      </mat-tab>
+                      <mat-tab label="CSS">
+                        <pre class="code">
+                          <code [innerHtml]="css_content"></code>
+                        </pre>
+                      </mat-tab>
+                    </mat-tab-group>
+                  </mat-expansion-panel>
+                </mat-accordion>
+
+                <div class="render">
+                  <mat-card>
+                      <mat-chip-list>
+                        <mat-chip>One fish</mat-chip>
+                        <mat-chip>Two fish</mat-chip>
+                        <mat-chip color="primary" selected="true">Primary fish</mat-chip>
+                        <mat-chip color="accent" selected="true">Accent fish</mat-chip>
+                      </mat-chip-list>
+                  </mat-card>
+                </div>
+
+              </mat-card-content>
+            </mat-tab>
+            <mat-tab label="API">Content 2</mat-tab>
+          </mat-tab-group>
         </article>
       </div>
     </main>
   `
 })
-export class AppComponent {}
+export class AppComponent {
+  languages = ['html', 'typescript', 'css'];
+  html_content = hljs.highlightAuto(`
+    <mat-chip-list>
+      <mat-chip>One fish</mat-chip>
+      <mat-chip>Two fish</mat-chip>
+      <mat-chip color="primary" selected="true">Primary fish</mat-chip>
+      <mat-chip color="accent" selected="true">Accent fish</mat-chip>
+    </mat-chip-list>`
+    , this.languages).value;
+
+  css_content = hljs.highlightAuto(`
+    @font-face {
+      font-family: Chunkfive; src: url('Chunkfive.otf');
+    }
+
+    body, .usertext {
+      color: #F0F0F0; background: #600;
+      font-family: Chunkfive, sans;
+    }
+
+    @import url(print.css);
+    @media print {
+      a[href^=http]::after {
+        content: attr(href)
+      }
+    })`, this.languages).value;
+
+  ts_content = hljs.highlightAuto(`
+    import {Component} from '@angular/core';
+
+    /**
+     * @title Basic expansion panel
+     */
+    @Component({
+      selector: 'expansion-overview-example',
+      templateUrl: 'expansion-overview-example.html',
+      styleUrls: ['expansion-overview-example.css'],
+    })
+    export class ExpansionOverviewExample {
+      panelOpenState: boolean = false;
+    }`, this.languages).value;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { SharedModule } from '@app/shared/shared.module'
 import { NgModule } from '@angular/core';
 import { CoreModule } from '@app/core/core.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router'
-import { MatListModule, MatTabsModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { MatListModule, MatTabsModule, MatExpansionModule, MatChipsModule, MatCardModule } from '@angular/material';
 import { AppRoutes } from './app.routing';
 
 @NgModule({
@@ -13,6 +13,10 @@ import { AppRoutes } from './app.routing';
   imports: [
     CoreModule,
     MatListModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatChipsModule,
+    MatCardModule,
     RouterModule.forRoot(AppRoutes),
   ],
   providers: [],
