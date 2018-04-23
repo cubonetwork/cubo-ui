f104d05cf301ad8b3acf13236667fbd9dfb20448
CarolPC <carolina.pascale.c@gmail.com>
2018-04-23 16:57:55 -0300

    Add example for cb-avatar

--- src/app/pages/avatar/avatar.component.ts
+++ src/app/pages/avatar/avatar.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import avatar from './avatar.js';
+import * as hljs from 'highlight.js';
+const languages = ['html', 'typescript', 'css'];
 
 @Component({
   selector: 'page-avatar-description',
   template: `
-    <p>Description</p>
+    <p>cb-avatar is made for using avatar images in your app. This directive supports images in (png, jpg, etc.). If you do not provide an image url, it uses a default one.</p>
+    <h2>Without image</h2>
+    <p>If a image is not set, the component expects a name property. Given this property, it renders the name initials as the avatar image.</p>
+    <h2>Input</h2>
+    <p>The cb-avatar can receive an image or a name input to display as an avatar.</p>
+    <h2>Size</h2>
+    <p>The selected image size can be changed by using size property. By default, the selected size is large. This can be changed to xsmall, small, medium, or xlarge.</p>
   `,
 })
 export class PageAvatarDescriptionComponent { }
@@ -11,31 +20,37 @@ export class PageAvatarDescriptionComponent { }
 @Component({
   selector: 'page-avatar-code-html',
   template: `
-    <p>HTML</p>
+    <code [innerHtml]="html_content"></code>
   `,
 })
-export class PageAvatarCodeHtmlComponent { }
+export class PageAvatarCodeHtmlComponent {
+  html_content = hljs.highlightAuto(avatar.html, languages).value;
+}
 
 @Component({
   selector: 'page-avatar-code-css',
   template: `
-    <p>CSS</p>
+    <code [innerHtml]="css_content"></code>
   `,
 })
-export class PageAvatarCodeCssComponent { }
+export class PageAvatarCodeCssComponent {
+  css_content = hljs.highlightAuto(avatar.css, languages).value;
+}
 
 @Component({
   selector: 'page-avatar-code-ts',
   template: `
-    <p>TS</p>
+    <code [innerHtml]="ts_content"></code>
   `,
 })
-export class PageAvatarCodeTsComponent { }
+export class PageAvatarCodeTsComponent {
+  ts_content = hljs.highlightAuto(avatar.ts, languages).value;
+}
 
 @Component({
   selector: 'page-avatar-render',
   template: `
-    <p>Render</p>
+    <cb-avatar [imageUrl]="'https://bit.ly/2vvFXCO'"></cb-avatar>
   `,
 })
 export class PageAvatarRenderComponent { }
