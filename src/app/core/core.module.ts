ade99144ac60ab51d37af8684aae875f2609f918
LFeh <hi@felipefialho.com>
2018-04-23 17:05:46 -0300

    Remove unuse module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,19 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule,
     BrowserModule,
-    BrowserAnimationsModule,
-    FlexLayoutModule
+    BrowserAnimationsModule
   ],
-  exports: [
-    FlexLayoutModule,
-  ],
+  exports: [ ],
   declarations: [],
   providers: [
   ]
