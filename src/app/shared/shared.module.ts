dddcb74f11ce55dae5a4dc6fe3bfcee3fffe308c
LFeh <hi@felipefialho.com>
2018-04-25 11:43:38 -0300

    Add angular-flex and router to shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule,
+    CommonModule,
+    FlexLayoutModule
   ],
   declarations: [ ],
   exports: [
-    CommonModule
+    RouterModule,
+    CommonModule,
+    FlexLayoutModule
   ]
 })
 export class SharedModule { }
