bee3ece61e37cb297a328cd859b2dbf0b7bab484
LFeh <hi@felipefialho.com>
2018-04-25 18:11:55 -0300

    Rename file

--- projects/application/app/core/core.module.ts
+++ projects/application/app/core/core.module.ts
@@ -4,7 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { MetaModule, MetaLoader } from '@ngx-meta/core';
-import { metaFactory } from '@app/core/meta.config';
+import { metaFactory } from '@app/core/core.meta.config';
 
 @NgModule({
   imports: [
