1d138ccf8cacb71ad19493e85ade794587c12f18
CarolPC <carolina.pascale.c@gmail.com>
2018-06-05 12:08:37 -0300

    Remove detect changes

--- projects/lib/src/cb-menu/cb-menu.component.ts
+++ projects/lib/src/cb-menu/cb-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy, Input, ChangeDetectorRef } from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, Input } from '@angular/core';
 
 /**
 * Component `<cb-menu>` to floating menu based on material ui
@@ -35,13 +35,11 @@ export class CbMenuComponent {
   @Input() backdrop = true;
   active = false;
 
-  constructor(private ref: ChangeDetectorRef) {}
+  constructor() {}
 
   toggleMenu(event) {
-    this.ref.detectChanges();
     event.stopPropagation();
     this.active = !this.active;
-    this.ref.detectChanges();
   }
 
   hiddenMenu(event) {
