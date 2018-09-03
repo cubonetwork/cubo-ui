2692653585fba7d42a0ea86250626d301893a8ac
LFeh <hi@felipefialho.com>
2018-09-03 16:30:02 -0300

    Add tests

---
+++ projects/lib/src/cb-chip/cb-chip.component.spec.ts
@@ -0,0 +1,157 @@
+import { MatIconModule } from '@angular/material';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+
+import { CbChipComponent, CbChipListComponent } from './cb-chip.component';
+
+describe('CbChipComponent', () => {
+  let comp: CbChipComponent;
+  let fixture: ComponentFixture<CbChipComponent>;
+  let element: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ MatIconModule ],
+      declarations: [ CbChipComponent, CbChipListComponent ],
+    });
+    fixture = TestBed.createComponent(CbChipComponent);
+    comp = fixture.componentInstance;
+  });
+
+  describe('when bg is...', () => {
+    it('true, should have the bg class', () => {
+      comp.bg = true;
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).toContain('chip--bg');
+    });
+
+    it('false, should not have the bg class', () => {
+      comp.bg = false;
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).not.toContain('chip--bg');
+    });
+  });
+
+  describe('when icon is...', () => {
+    it('github, should have the github icon', () => {
+      comp.icon = 'github';
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement.querySelector('mat-icon');
+
+      expect(element.getAttribute('ng-reflect-svg-icon')).toContain('github');
+    });
+
+    it('a valid value, should have the icon class', () => {
+      comp.icon = 'github';
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).toContain('chip--icon');
+    });
+
+    it('undefined, should not have the icon class', () => {
+      comp.icon = undefined;
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).not.toContain('chip--icon');
+    });
+  });
+
+  describe('when size is...', () => {
+    it('small, should have the small class', () => {
+      comp.size = 'small';
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).toContain('chip--size-small');
+    });
+
+    it('medium, should have the medium class', () => {
+      comp.size = 'medium';
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).toContain('chip--size-medium');
+    });
+
+    it('large, should have the large class', () => {
+      comp.size = 'large';
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).toContain('chip--size-large');
+    });
+  });
+
+  describe('when color is...', () => {
+    it('blue, should have the blue class', () => {
+      comp.color = 'blue';
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).toContain('chip--color-blue');
+    });
+
+    it('orange, should have the orange class', () => {
+      comp.color = 'orange';
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).toContain('chip--color-orange');
+    });
+
+    it('green, should have the green class', () => {
+      comp.color = 'green';
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).toContain('chip--color-green');
+    });
+
+    it('gray-light, should have the gray-light class', () => {
+      comp.color = 'gray-light';
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).toContain('chip--color-gray-light');
+    });
+  });
+});
+
+describe('CbChipListComponent', () => {
+  let comp: CbChipListComponent;
+  let fixture: ComponentFixture<CbChipListComponent>;
+  let element: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ MatIconModule ],
+      declarations: [ CbChipComponent, CbChipListComponent ],
+    });
+    fixture = TestBed.createComponent(CbChipListComponent);
+    comp = fixture.componentInstance;
+  });
+
+  describe('when direction is...', () => {
+    it('horizontal, should have the horizontal class', () => {
+      comp.direction = 'horizontal';
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).toContain('list--h');
+    });
+
+    it('vertical, should have the vertical class', () => {
+      comp.direction = 'vertical';
+      fixture.detectChanges();
+      element = fixture.debugElement.nativeElement;
+
+      expect(element.getAttribute('class')).toContain('list--v');
+    });
+  });
+});
