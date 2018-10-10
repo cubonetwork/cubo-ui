dd595077673f1e7e906cc79d3167b639cae2fa75
LFeh <hi@felipefialho.com>
2018-10-10 19:32:34 -0300

    Add preview to image

--- projects/lib/src/cb-upload-image/cb-upload-image.component.ts
+++ projects/lib/src/cb-upload-image/cb-upload-image.component.ts
@@ -2,8 +2,11 @@ import {
   Component,
   ViewEncapsulation,
   ChangeDetectionStrategy,
-  Input
+  Input,
+  OnInit,
+  ChangeDetectorRef
 } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 /**
  * Component `<cb-upload-image>` to make a share action
@@ -13,19 +16,56 @@ import {
   template: `
     <div class="upload">
       <label class="upload__button">
-        <input class="upload__input" type="file">
+        <input class="upload__input" type="file" (change)="uploadImage($event)">
         {{ label }}
       </label>
     </div>
-    <div class="preview"></div>
+    <div class="preview" [style.background-image]="_bg" [style.height]="_height" [style.width]="_width" [style.border-radius]="_borderRadius">
+      <img class="preview__image" [src]="_image" *ngIf="_image" [style.object-fit]="fit">
+    </div>
   `,
   styleUrls: ['./cb-upload-image.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.Default,
   host: {
     'class': 'cb-upload-image'
   }
 })
-export class CbUploadImageComponent {
+export class CbUploadImageComponent implements OnInit {
   @Input() label = 'Upload image';
+  @Input() bg: string;
+  @Input() image: string;
+  @Input() size = '64';
+  @Input() fit: 'contain' | 'cover' = 'contain';
+  @Input() type: 'square' | 'circle' = 'square';
+
+  _bg: SafeStyle;
+  _image: SafeStyle;
+  _borderRadius: string;
+  _height: string;
+  _width: string;
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private ref: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this._bg = this.bg ? this.sanitizer.bypassSecurityTrustStyle(`url(${this.bg})`) : '';
+    this._image = this.image ? this.sanitizer.bypassSecurityTrustStyle(`url(${this.image})`) : '';
+    this._borderRadius = this.type === 'circle' ? '50%' : '0';
+    this._height = `${this.size}px`;
+    this._width = this.type === 'circle' ? `${this.size}px` : 'auto';
+  }
+
+  uploadImage(upload) {
+    if (upload.target.files && upload.target.files[0]) {
+      const reader = new FileReader();
+      reader.readAsDataURL(upload.target.files[0]);
+      reader.onload = (e: any) => {
+        this._image = e.target.result;
+        this.ref.detectChanges();
+      };
+    }
+  }
 }
