b14b4c37fd49044f8c383af36d5a712fea2b6103
LFeh <hi@felipefialho.com>
2019-02-06 20:05:25 -0200

    Remove mat-button

--- projects/application/src/app/pages/page-components/page-highlight/page-highlight.module.ts
+++ projects/application/src/app/pages/page-components/page-highlight/page-highlight.module.ts
@@ -2,19 +2,18 @@ import { CbHighlightModule } from '@lib';
 import { ContentModule } from '@app/components/content/content.module';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@app/shared/shared.module';
-import { MatButtonModule } from '@angular/material';
 
 import { PageHighlightRouting } from './page-highlight.routing';
+
 import {
   PageHighlightExampleComponent,
   PageHighlightApiComponent,
-  PageHighlightComponent
+  PageHighlightComponent,
 } from './page-highlight.component';
 
 @NgModule({
   imports: [
     SharedModule,
-    MatButtonModule,
     PageHighlightRouting,
     CbHighlightModule,
     ContentModule
