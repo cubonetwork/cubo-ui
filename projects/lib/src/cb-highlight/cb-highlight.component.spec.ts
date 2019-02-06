c4b19e58ab7393299dd898899e9f635305f68285
LFeh <hi@felipefialho.com>
2019-02-06 15:00:05 -0200

    Add tests

---
+++ projects/lib/src/cb-highlight/cb-highlight.component.spec.ts
@@ -0,0 +1,55 @@
+import { MatIconModule } from '@angular/material';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+
+import { CbHighlightComponent } from './cb-highlight.component';
+
+describe('CbHighlightComponent', () => {
+  let comp: CbHighlightComponent;
+  let fixture: ComponentFixture<CbHighlightComponent>;
+  let element: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ MatIconModule ],
+      declarations: [ CbHighlightComponent ],
+    });
+    fixture = TestBed.createComponent(CbHighlightComponent);
+    comp = fixture.componentInstance;
+  });
+
+  describe('when position is left', () => {
+    beforeEach(() => {
+      comp.position = 'left';
+      fixture.detectChanges();
+    });
+
+    it('should have the position left class', () => {
+      element = fixture.debugElement.nativeElement.querySelector('.dialog');
+      expect(element.getAttribute('class')).toContain('dialog--left');
+    });
+  });
+
+  describe('when position is right', () => {
+    beforeEach(() => {
+      comp.position = 'right';
+      fixture.detectChanges();
+    });
+
+    it('should have the position right class', () => {
+      element = fixture.debugElement.nativeElement.querySelector('.dialog');
+      expect(element.getAttribute('class')).toContain('dialog--right');
+    });
+  });
+
+  describe('when dialog is active', () => {
+    beforeEach(() => {
+      comp.active = true;
+      fixture.detectChanges();
+    });
+
+    it('should have the active class', () => {
+      element = fixture.debugElement.nativeElement.querySelector('.dialog');
+      expect(element.getAttribute('class')).toContain('dialog--active');
+    });
+  });
+});
