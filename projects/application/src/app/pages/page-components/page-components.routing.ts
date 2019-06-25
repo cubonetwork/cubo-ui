26d3a9c3275648fd363654d09f6882ad61a9bed4
LFeh <hi@felipefialho.com>
2019-06-25 10:36:59 -0300

    change routing to angular 8

--- projects/application/src/app/pages/page-components/page-components.routing.ts
+++ projects/application/src/app/pages/page-components/page-components.routing.ts
@@ -13,7 +13,7 @@ export const PageComponentsRoutes: Routes = [
     children: [
       {
         path: '',
-        loadChildren: '@app/pages/page-components/page-home/page-home.module#PageHomeModule',
+        loadChildren: () => import('@app/pages/page-components/page-home/page-home.module').then(m => m.PageHomeModule),
         data: {
           meta: { title: 'Home' }
         }
@@ -24,70 +24,70 @@ export const PageComponentsRoutes: Routes = [
         children: [
           {
             path: 'accordion',
-            loadChildren: '@app/pages/page-components/page-accordion/page-accordion.module#PageAccordionModule',
+            loadChildren: () => import('@app/pages/page-components/page-accordion/page-accordion.module').then(m => m.PageAccordionModule),
             data: {
               meta: { title: 'Accordion' }
             }
           },
           {
             path: 'avatar',
-            loadChildren: '@app/pages/page-components/page-avatar/page-avatar.module#PageAvatarModule',
+            loadChildren: () => import('@app/pages/page-components/page-avatar/page-avatar.module').then(m => m.PageAvatarModule),
             data: {
               meta: { title: 'Avatar' }
             }
           },
           {
             path: 'card',
-            loadChildren: '@app/pages/page-components/page-card/page-card.module#PageCardModule',
+            loadChildren: () => import('@app/pages/page-components/page-card/page-card.module').then(m => m.PageCardModule),
             data: {
               meta: { title: 'Card' }
             }
           },
           {
             path: 'carousel',
-            loadChildren: '@app/pages/page-components/page-carousel/page-carousel.module#PageCarouselModule',
+            loadChildren: () => import('@app/pages/page-components/page-carousel/page-carousel.module').then(m => m.PageCarouselModule),
             data: {
               meta: { title: 'Carousel' }
             }
           },
           {
             path: 'chip',
-            loadChildren: '@app/pages/page-components/page-chip/page-chip.module#PageChipModule',
+            loadChildren: () => import('@app/pages/page-components/page-chip/page-chip.module').then(m => m.PageChipModule),
             data: {
               meta: { title: 'Chip' }
             }
           },
           {
             path: 'flag',
-            loadChildren: '@app/pages/page-components/page-flag/page-flag.module#PageFlagModule',
+            loadChildren: () => import('@app/pages/page-components/page-flag/page-flag.module').then(m => m.PageFlagModule),
             data: {
               meta: { title: 'Flag' }
             }
           },
           {
             path: 'highlight',
-            loadChildren: '@app/pages/page-components/page-highlight/page-highlight.module#PageHighlightModule',
+            loadChildren: () => import('@app/pages/page-components/page-highlight/page-highlight.module').then(m => m.PageHighlightModule),
             data: {
               meta: { title: 'Highlight' }
             }
           },
           {
             path: 'menu',
-            loadChildren: '@app/pages/page-components/page-menu/page-menu.module#PageMenuModule',
+            loadChildren: () => import('@app/pages/page-components/page-menu/page-menu.module').then(m => m.PageMenuModule),
             data: {
               meta: { title: 'Menu' }
             }
           },
           {
             path: 'social',
-            loadChildren: '@app/pages/page-components/page-social/page-social.module#PageSocialModule',
+            loadChildren: () => import('@app/pages/page-components/page-social/page-social.module').then(m => m.PageSocialModule),
             data: {
               meta: { title: 'Social' }
             }
           },
           {
             path: 'upload-image',
-            loadChildren: '@app/pages/page-components/page-upload-image/page-upload-image.module#PageUploadImageModule',
+            loadChildren: () => import('@app/pages/page-components/page-upload-image/page-upload-image.module').then(m => m.PageUploadImageModule),
             data: {
               meta: { title: 'UploadImage' }
             }
